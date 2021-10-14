74d252992f5ae9e3d444c3d6aede9ba371091a23
Sean Osborne <srosborn@ucsc.edu>
2021-10-13 18:06:30 -0700

    Edited index.html and lab.js files for lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,32 @@
+// These comments will show you how to start a manual car and code the card game War with betting.
+// Description. (use period)
+// @link srosborn.github.io/art101
+// @file   lab4/js/lab.js
+// @author Sean Osborne
+// @since  October 13, 2021
+
+//How to Start a Manual Car
+  //1. Use key to unlock the car door
+  //2. Get inside the car
+  //3. Put key into the ignition
+    //Make sure one foot is engaging the break, and that one foot is engaging the clutch
+  //4. If key is in ignition;
+    //Turn right
+  //5. Set car gear into 1st
+  //6. While pressing down on gas:
+    //Slowly come off clutch
+    //This may take practice so be patient!
+  //7. If the car is moving and your foot is fully off the clutch:
+    //Disengage the gas
+    //Engage clutch again
+  //8. While step 4 is repeating:
+    //Set gear to 2nd
+-----------------------------------------------------------------------------------------------------------------
+//Pseudocoding the card game War (with gambling)
+  //1. Randomly select a card for the player and for the computer before each round.
+  //2. Accept user input of amount they would like to bet, or deciding to fold.
+    //If player chooses fold, reset the board
+  //3. Computer calls after a certain card threshold.
+    //More advanced systems can chose to call based on card value and amount the player has bet
+  //4. Test of win or lose condition.
+  //5. Repeat from step 1.
