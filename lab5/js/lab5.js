85e87fb59f53963d04b278c5c1c938ec60813cec
Sean Osborne <srosborn@ucsc.edu>
2021-10-18 16:36:31 -0700

    Completed lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Sean Osborne
+ * Created:   10.18.2021
+ *
+ * License: Public Domain.
+ **/
+
+
+//Variables
+make = "Honda";
+model = "Pilot";
+year= 2005;
+color = "Silver";
+
+age = 2021 - year;
+
+//Output Variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
